0af63e914630ea35b1782bea34ac4fed6d873b75
DzinaKor <dinakor1922@gmail.com>
2022-09-04 18:28:32 +0400

    fix: try to fix path

--- src/Controllers/UserQuery.ts
+++ src/Controllers/UserQuery.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import User from '../Models/User';
+import User from './User';
 //const { User } = require('../Models/User');
 import { typeDBUser } from '../types';
 
