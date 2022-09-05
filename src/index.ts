10d387fb2c60368620f9057f19509eb886aab238
DzinaKor <dinakor1922@gmail.com>
2022-09-05 20:10:47 +0400

    refactor: temporary remove files

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { App } from './app';
-// const config = require( './config.json' );
-
-let app = new App();
-app.expressApp.listen(Number(process.env.PORT) || 3000, "0.0.0.0", function() {  // 8080, "0.0.0.0", 
-    console.log(`App listening at port ${3000}`);
-});
