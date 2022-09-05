1a8614a7bd64ba38e40482468ec03f19a8da1d68
DzinaKor <dinakor1922@gmail.com>
2022-09-05 20:36:35 +0400

    refactor: remove extra comments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { App } from './app';
-// const config = require( './config.json' );
 
 let app = new App();
 app.expressApp.listen(Number(process.env.PORT) || 3000, "0.0.0.0", function() {  // 8080, "0.0.0.0", 
--- src/user.ts
+++ src/user.ts
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose';
 import { typeDBUser, typeUser } from './types';
 
 export default class User {
